3c2dec08fa1c8681dc43a271348e7d695a80fd33
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-03-24 15:21:47 +0800

    added django.contrib.auth

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+LOGIN_REDIRECT_URL = "/merchstore/items"
+LOGOUT_REDIRECT_URL = "/accounts/login"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
