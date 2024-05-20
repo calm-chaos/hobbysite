b507bdc986a45dd612e221a67cdb7fad79e06227
jiyana <157709090+calm-chaos@users.noreply.github.com>
2024-05-21 07:46:34 +0800

    switched order of accounts url and merchstore url

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("merchstore/", include("merchstore.urls", namespace="merchstore")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("merchstore/", include("merchstore.urls", namespace="merchstore")),
 ]
