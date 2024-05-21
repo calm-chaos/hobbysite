e651b4e4273eea95dadd7bf5e9a982ddf317b39a
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-21 22:13:45 +0800

    created views and templates

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import *
 
-# Create your views here.
+
+class ForumListView(ListView):
+    model = Post
+    template_name = "forum_list.html"
+
+
+class ForumDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = "forum_detail.html"
