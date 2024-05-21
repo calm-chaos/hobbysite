cbea1f3c250250149fea086ab0dfabc7f5b2a20e
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-21 22:12:00 +0800

    created postcategory and post models

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "Post Categories"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        "PostCategory", null=True, on_delete=models.SET_NULL, related_name="posts"
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}".format(self.title)
+
+    def get_absolute_url(self):
+        return reverse("forum:thread-detail", args=[self.pk])
+
+    class Meta:
+        ordering = ["-created_on"]
