17655e8467f09c5873ca78fdcb44608f7a756075
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-21 22:43:12 +0800

    added path to accounts

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("forum/", include("forum.urls", namespace="forum")),
 ]
