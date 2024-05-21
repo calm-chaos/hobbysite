7f3b077452bf1168fa62698b5f9a6da22192f057
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-21 22:16:37 +0800

    created admin models

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Post, PostAdmin)
