aa541f696c9d83e14b9ff72934d777ee375ff77d
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-22 15:12:29 +0800

    created commission and comment models

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    required_ppl = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}".format(self.title)
+
+    def get_absolute_url(self):
+        return reverse("commissions:comm-detail", args=[self.pk])
+
+    class Meta:
+        ordering = ["created_on"]
+
+
+class Comment(models.Model):
+    commission = models.ForeignKey(
+        "Commission", null=True, on_delete=models.CASCADE, related_name="commissions"
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
