7fd0bff0b4cbc9d44bf330058bd7b7c0656ebc9a
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-22 15:15:34 +0800

    created admin panels

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Comment, CommentAdmin)
