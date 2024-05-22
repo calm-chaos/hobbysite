adb176f839bce7187532b500fe8b05a06d8a74b5
calm-chaos <jianna.corpuz@student.ateneo.edu>
2024-05-22 15:13:27 +0800

    created views for commission list and detail

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import *
 
-# Create your views here.
+
+class CommissionListView(ListView):
+    model = Commission
+    template_name = "commission_list.html"
+
+
+class CommissionDetailView(DetailView):
+    model = Commission
+    template_name = "commission_detail.html"
